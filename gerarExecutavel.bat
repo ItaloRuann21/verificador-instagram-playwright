d1578b01d6c8ffe7a499c95dc9c4edf8238ff0c3
ItaloRuann21 <italotavares751@gmail.com>
2024-02-14 12:29:33 -0300

    update

--- gerarExecutavel.bat
+++ gerarExecutavel.bat
@@ -2,4 +2,4 @@ pyinstaller --noconsole --onefile --icon=./storage/img/irvtbot.png interface_log
 xcopy /s /i /e /q /y .\relatorios .\dist\relatorios\
 xcopy /s /i /e /q /y .\storage .\dist\storage\
 xcopy /s /i /e /q /y .\logs .\dist\logs\
-xcopy /y .\useragents.txt .\dist\
+xcopy /y .\useragents.txt .\dist\storage\config
