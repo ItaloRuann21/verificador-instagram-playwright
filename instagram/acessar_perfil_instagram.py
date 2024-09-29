67e14666df426155fba990176b6e9ff78131613c
ItaloRuann21 <italotavares751@gmail.com>
2024-09-29 03:46:43 -0300

    update

--- instagram/acessar_perfil_instagram.py
+++ instagram/acessar_perfil_instagram.py
@@ -6,34 +6,36 @@ from mensagens.mensagens_coloridas import (mensagem_erro, mensagem_normal,
 def acessar_perfil_instagram(pagina, usuario, senha):
     try:
         mensagem_normal(f'{usuario} - Fazendo login no Instagram')
-        
+
         # Site instagram
         pagina.goto('https://www.instagram.com/')
-        
+
         # Definindo usuário e senha do instagram
         pagina.wait_for_selector('[name="username"]')
         pagina.fill('[name="username"]', usuario)
         pagina.wait_for_timeout(2000)
         pagina.wait_for_selector('[name="password"]')
         pagina.fill('[name="password"]', senha)
-        
+
         # Confirmando credenciais
         pagina.wait_for_selector('text="Entrar"')
         pagina.wait_for_timeout(2000)
         pagina.click('text="Entrar"')
-        
+
         # Esperando o seletor do span página inicial aparecer
-        pagina.wait_for_selector('span:has-text("Página inicial")')
-        
+        pagina.wait_for_selector(
+            'span:has-text("Página inicial")', timeout=60000)
+
         contas_ativas(usuario, senha)
-        
+
         return True
     except Exception as e:
         mensagem_erro(f'Erro ao fazer login: {str(e)}')
         nao_fez_login(usuario, senha)
-        
+
         # Gravando o erro em um arquivo de log
         with open('./logs/instagram_error.txt', 'a+') as arquivo:
-            arquivo.write(f'Erro na arquivo acessar_perfil_instagram.py. Código: {str(e)}\n\n')
-        
+            arquivo.write(
+                f'Erro na arquivo acessar_perfil_instagram.py. Código: {str(e)}\n\n')
+
         return False
